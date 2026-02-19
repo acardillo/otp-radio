b0605bb6739c85bc5bfabb69aa9718068f3e1f6f
Anthony Cardillo <acardillo.canada@gmail.com>
2026-02-19 15:47:28 -0500

    Add public static page for /listener.html for connecting to stream

--- lib/otp_radio_web.ex
+++ lib/otp_radio_web.ex
@@ -17,7 +17,7 @@ defmodule OtpRadioWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt listener.html)
 
   def router do
     quote do
